7fc1c77c9e5638155d03cb70f4f8447ce41b5276
a7medCyber <ahmed1alghamdi2000@gmail.com>
2021-09-01 14:15:10 +0300

    whitenoise

--- tqyeem/settings.py
+++ tqyeem/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'tqyeem_app.apps.TqyeemAppConfig',
     
@@ -125,6 +126,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
